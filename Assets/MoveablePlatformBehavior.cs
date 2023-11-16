95d4ee55ad80cf7295414c2de94f890b7b7776e0
KOLIP-LAPTOP\GBC <sinan.kolip@georgebrown.ca>
2023-11-16 14:44:37 -0500

    Custom movement from path to path for platform added

--- Assets/MoveablePlatformBehavior.cs
+++ Assets/MoveablePlatformBehavior.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class MoveablePlatformBehavior : MonoBehaviour
@@ -16,17 +17,68 @@ public class MoveablePlatformBehavior : MonoBehaviour
     [SerializeField]
     float _verticalDistance = 3;
 
+    [SerializeField]
+    List<Transform> _pathTransforms = new List<Transform>();
+
+    List<Vector2> _destinations = new List<Vector2>();
+
     Vector2 _startPosition;
+    Vector2 _endPosition;
+
+    int _destinationIndex = 0;
+    float _timer;
+    [SerializeField]
+    [Range(0, 0.1f)]
+    float _customMovementTimeChangeFactor = 0.05f;
     // Start is called before the first frame update
     void Start()
     {
+
+        foreach(Transform t in _pathTransforms)
+        {
+            _destinations.Add(t.position);
+        }
+
+        _destinations.Add(transform.position);
+
         _startPosition = transform.position;
+        _endPosition = _destinations[_destinationIndex];
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_types == PlatformMovementTypes.CUSTOM)
+        {
+            if (_timer >= 1) // This mean i reached to my destination.
+            {
+                _timer = 0;
+                _destinationIndex++;
+
+                if (_destinationIndex >= _destinations.Count)
+                {
+                    _destinationIndex = 0;
+                }
+
+                _startPosition = transform.position;
+                _endPosition = _destinations[_destinationIndex];
+
+            }
+
+            else if(_timer < 1)
+            {
+                _timer += _customMovementTimeChangeFactor;
+            } 
+
+
+        }
+
     }
 
 
@@ -51,6 +103,7 @@ public class MoveablePlatformBehavior : MonoBehaviour
                                                  Mathf.PingPong(_verticalSpeed * Time.time, _verticalDistance) + _startPosition.y);
                 break;
             case PlatformMovementTypes.CUSTOM:
+                transform.position = Vector2.Lerp(_startPosition,_endPosition,_timer );
                 break;
         }
     }
