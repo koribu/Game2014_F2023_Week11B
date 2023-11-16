ba66e6dcc6da4647e3f25a655b300bdb5b21949d
KOLIP-LAPTOP\GBC <sinan.kolip@georgebrown.ca>
2023-11-16 13:28:15 -0500

    Checkpoint and deathplane behavior added

--- Assets/[Scripts]/PlayerBehavior.cs
+++ Assets/[Scripts]/PlayerBehavior.cs
@@ -149,14 +149,6 @@ public class PlayerBehavior : MonoBehaviour
 
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-
-        if (collision.CompareTag("DeathPlane"))
-        {
-            transform.position = new Vector3(0, 5, 0) ;
-        }
-    }
 
 
 
