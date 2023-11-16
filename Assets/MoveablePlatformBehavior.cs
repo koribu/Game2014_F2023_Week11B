f5b72508bbb8d681293e91807db9f23ef2152388
KOLIP-LAPTOP\GBC <sinan.kolip@georgebrown.ca>
2023-11-16 14:13:53 -0500

    Landing on platforms added

--- Assets/MoveablePlatformBehavior.cs
+++ Assets/MoveablePlatformBehavior.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class MoveablePlatformBehavior : MonoBehaviour
 {
     [SerializeField]
+    PlatformMovementTypes _types;
+
+    [SerializeField]
     float _horizontalSpeed = 5;
     [SerializeField]
+    float _verticalSpeed = 3;
+    [SerializeField]
     float _horizontalDistance = 5;
+    [SerializeField]
+    float _verticalDistance = 3;
 
     Vector2 _startPosition;
     // Start is called before the first frame update
@@ -19,7 +26,32 @@ public class MoveablePlatformBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(Mathf.PingPong(_horizontalSpeed * Time.time, _horizontalDistance) + _startPosition.x,
-                                          transform.position.y);   
+        Move();
+    }
+
+
+    void Move()
+    {
+        switch(_types)
+        {
+            case PlatformMovementTypes.HORIZONTAL:
+                transform.position = new Vector2(Mathf.PingPong(_horizontalSpeed * Time.time, _horizontalDistance) + _startPosition.x,
+                                         transform.position.y);
+                break;
+            case PlatformMovementTypes.VERTICAL:
+                transform.position = new Vector2(transform.position.x,
+                                                 Mathf.PingPong(_verticalSpeed * Time.time, _verticalDistance) + _startPosition.y);
+                break;
+            case PlatformMovementTypes.DIAGONAL_RIGHT:
+                transform.position = new Vector2(Mathf.PingPong(_horizontalSpeed * Time.time, _horizontalDistance) + _startPosition.x,
+                                                  Mathf.PingPong(_verticalSpeed * Time.time, _verticalDistance) + _startPosition.y);
+                break;
+            case PlatformMovementTypes.DIAGONAL_LEFT:
+                transform.position = new Vector2(_startPosition.x - Mathf.PingPong(_horizontalSpeed * Time.time, _horizontalDistance) ,
+                                                 Mathf.PingPong(_verticalSpeed * Time.time, _verticalDistance) + _startPosition.y);
+                break;
+            case PlatformMovementTypes.CUSTOM:
+                break;
+        }
     }
 }
